bd6b6e84496f4d357b961371ae5673843429ac2e
chenghao <473726199@qq.com>
2024-03-08 18:32:05 +0800

    ⚡️perf: 优化按钮实现

--- lib/src/super_button.dart
+++ lib/src/super_button.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
+/// @author : ch
+/// @date 2024-03-08 16:52:29
+/// @description test
+///
+///
 enum ButtonType { filled, outlined, outlinedPrimary, text, textPrimary }
 
-/// @author : ch
-/// @description 按钮
-///
 class SuperButton extends StatelessWidget {
-  /// 按钮点击的回调
-  final VoidCallback onTap;
-
   /// 按钮显示的文案
   final ButtonType type;
 
   /// 按钮显示的文案
-  final String? text;
+  final String text;
+
+  /// 按钮点击的回调
+  final VoidCallback onTap;
 
   /// 按钮的文字颜色
   final Color? textColor;
@@ -38,46 +40,35 @@ class SuperButton extends StatelessWidget {
   /// 按钮背景色
   final Color? backgroundColor;
 
-  /// 按钮不可用的文字颜色
-  final Color? disableTextColor;
-
-  /// 按钮不可用背景色
-  final Color? disableBackgroundColor;
-
   /// 按钮外边距
   final EdgeInsetsGeometry? margin;
 
   /// 按钮内边距
   final EdgeInsetsGeometry? padding;
-  final double? paddingHorizontal;
-  final double? paddingVertical;
-
-  /// 盒子阴影
-  final List<BoxShadow>? boxShadow;
-
-  /// 按钮的修饰 默认实色背景
-  final Decoration? decoration;
-
-  /// 按钮的布局约束 默认是自适应大小
-  final BoxConstraints? constraints;
+  final double paddingHorizontal;
+  final double paddingVertical;
 
   /// 按钮的内部对齐 默认为null
   final Alignment? alignment;
 
-  /// 按钮圆角大小
+  /// 按钮圆角大小,
   final double borderRadius;
 
-  /// 边框宽度
-  final double borderWidth;
+  /// 按钮边框颜色，borderWidth不能为空生效
+  /// [ButtonType.outlinedPrimary]默认为主题色
+  final Color? borderColor;
+
+  /// 按钮边框宽度，borderColor不能为空生效
+  final double? borderWidth;
 
   /// 按钮是否可用 默认是 [true]
   final bool enabled;
 
   const SuperButton({
-    Key? key,
-    required this.onTap,
+    super.key,
     required this.type,
-    this.text,
+    required this.text,
+    required this.onTap,
     this.textColor,
     this.textSize,
     this.textStyle,
@@ -87,93 +78,104 @@ class SuperButton extends StatelessWidget {
     this.prefixWidget,
     this.suffixWidget,
     this.backgroundColor,
-    this.disableTextColor,
-    this.disableBackgroundColor = const Color(0xFFCCCCCC),
     this.margin,
     this.padding,
-    this.paddingHorizontal,
-    this.paddingVertical,
-    this.boxShadow,
-    this.decoration,
-    this.constraints,
+    this.paddingHorizontal = 8,
+    this.paddingVertical = 6,
     this.alignment,
     this.borderRadius = 6,
-    this.borderWidth = 0.5,
+    this.borderWidth,
+    this.borderColor,
     this.enabled = true,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
-    Color? backgroundColor = this.backgroundColor;
-    Color? textColor = this.textColor;
-    Color? disableTextColor = this.disableTextColor;
-    Color primaryColor = Theme.of(context).primaryColor;
-    switch (type) {
-      case ButtonType.filled:
-        backgroundColor = backgroundColor ?? primaryColor;
-        textColor = textColor ?? Colors.white;
-        disableTextColor = disableTextColor ?? const Color(0xFF666666);
-        break;
-      case ButtonType.outlinedPrimary:
-      case ButtonType.textPrimary:
-        textColor = textColor ?? primaryColor;
-        disableTextColor = textColor.withOpacity(0.2);
-        break;
-      case ButtonType.outlined:
-      case ButtonType.text:
-        textColor = textColor ?? const Color(0xFF666666);
-        break;
-    }
+    OutlinedBorder border = RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius));
+    EdgeInsetsGeometry padding = this.padding ?? EdgeInsets.symmetric(vertical: paddingVertical, horizontal: paddingHorizontal);
+    Color? textColor = getTextColor(Theme.of(context).primaryColor);
+    Widget buttonWidget = const SizedBox.shrink();
 
-    return Material(
-      borderRadius: BorderRadius.circular(borderRadius),
-      color: Colors.transparent,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(borderRadius),
-        onTap: () {
-          if (enabled) onTap.call();
-        },
-        child: Container(
-          width: width,
-          height: height,
-          margin: margin,
-          alignment: alignment ?? (width != null ? Alignment.center : null),
-          decoration: decoration ??
-              BoxDecoration(
-                color: enabled ? backgroundColor : disableBackgroundColor,
-                border: (type == ButtonType.outlined || type == ButtonType.outlinedPrimary)
-                    ? Border.all(color: type == ButtonType.outlinedPrimary ? Theme.of(context).primaryColor : const Color(0xFF999999), width: borderWidth)
-                    : null,
-                borderRadius: BorderRadius.circular(borderRadius),
-                boxShadow: boxShadow,
-              ),
-          constraints: constraints,
-          padding: EdgeInsets.symmetric(vertical: paddingVertical ?? 6, horizontal: paddingHorizontal ?? 8),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              if (prefixWidget != null) prefixWidget!,
-              if (prefixWidget != null) SizedBox(width: widgetMarginHorizontal),
-              Text(text ?? "", overflow: TextOverflow.ellipsis, maxLines: 1, style: _getTextStyle(textColor), textAlign: TextAlign.center),
-              if (suffixWidget != null) SizedBox(width: widgetMarginHorizontal),
-              if (suffixWidget != null) suffixWidget!,
-            ],
-          ),
+    if (type == ButtonType.filled) {
+      buttonWidget = FilledButton(
+        onPressed: enabled ? onTap : null,
+        child: getChildren(textColor),
+        style: FilledButton.styleFrom(
+          backgroundColor: backgroundColor,
+          padding: padding,
+          alignment: alignment,
+          shape: border,
+          side: borderWidth != null && borderColor != null ? BorderSide(width: borderWidth!, color: borderColor!) : null,
         ),
-      ),
+      );
+    } else if (type == ButtonType.outlined || type == ButtonType.outlinedPrimary) {
+      buttonWidget = OutlinedButton(
+        onPressed: enabled ? onTap : null,
+        child: getChildren(textColor),
+        style: FilledButton.styleFrom(
+          backgroundColor: backgroundColor,
+          padding: padding,
+          alignment: alignment,
+          shape: border,
+          side: enabled && ((borderWidth != null && borderColor != null) || type == ButtonType.outlinedPrimary)
+              ? BorderSide(width: borderWidth ?? 1, color: borderColor ?? Theme.of(context).primaryColor)
+              : null,
+        ),
+      );
+    } else if (type == ButtonType.text || type == ButtonType.textPrimary) {
+      buttonWidget = TextButton(
+        onPressed: enabled ? onTap : null,
+        child: getChildren(textColor),
+        style: TextButton.styleFrom(
+          backgroundColor: backgroundColor,
+          padding: padding,
+          alignment: alignment,
+          shape: border,
+          side: borderWidth != null && borderColor != null ? BorderSide(width: borderWidth!, color: borderColor!) : null,
+        ),
+      );
+    }
+    return Container(
+      width: width,
+      height: height,
+      margin: margin,
+      child: buttonWidget,
     );
   }
 
-  TextStyle _getTextStyle(Color? textColor) {
-    if (textStyle != null) {
-      return textStyle!;
-    }
-    Color? fontColor = enabled ? textColor : disableTextColor;
-    return TextStyle(
-      fontSize: textSize,
-      color: fontColor,
+  getChildren(Color? textColor) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        if (prefixWidget != null) prefixWidget!,
+        if (prefixWidget != null) SizedBox(width: widgetMarginHorizontal),
+        Flexible(
+          child: Text(
+            text ?? "",
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: textStyle?.copyWith(color: textColor, fontSize: textSize) ??
+                TextStyle(
+                  color: textColor,
+                  fontSize: textSize,
+                ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        if (suffixWidget != null) SizedBox(width: widgetMarginHorizontal),
+        if (suffixWidget != null) suffixWidget!,
+      ],
     );
+  }
+
+  Color? getTextColor(Color primaryColor) {
+    if (!enabled) return null;
+    Color? textColor = this.textColor;
+    if (type == ButtonType.outlined || type == ButtonType.text) {
+      textColor = textColor ?? const Color(0xFF666666);
+    }
+    return textColor;
   }
 }
