d19d54364b30d5d427947eeb16729bd03dd5f87c
chenghao <473726199@qq.com>
2024-08-05 10:07:45 +0800

    ✨feat: 导入 extended_sliver 2.1.3

--- lib/super_ui.dart
+++ lib/super_ui.dart
@@ -1,6 +1,10 @@
 library super_ui;
 
+export 'src/load/super_load.dart';
+export 'src/load/super_load_page.dart';
+export 'src/load/super_load_status.dart';
 export 'src/screen.dart';
+export 'src/sliver/widget.dart';
 export 'src/super_body.dart';
 export 'src/super_button.dart';
 export 'src/super_card.dart';
@@ -8,14 +12,11 @@ export 'src/super_divider.dart';
 export 'src/super_expandable_text.dart';
 export 'src/super_icon.dart';
 export 'src/super_image.dart';
-export 'src/super_item_text.dart';
 export 'src/super_item.dart';
+export 'src/super_item_text.dart';
 export 'src/super_keep_wrapper.dart';
 export 'src/super_popup.dart';
+export 'src/super_rich_text.dart';
 export 'src/super_text.dart';
 export 'src/super_text_filed.dart';
 export 'src/super_visibility.dart';
-export 'src/super_rich_text.dart';
-export 'src/load/super_load.dart';
-export 'src/load/super_load_page.dart';
-export 'src/load/super_load_status.dart';
