a559832fea420182a3f39284ae2f152bc9d871b1
chenghao <473726199@qq.com>
2024-10-25 14:00:30 +0800

    ⚡️perf: 输入框正则优化

--- lib/src/super_text_filed.dart
+++ lib/src/super_text_filed.dart
@@ -69,8 +69,8 @@ class SuperTextFiled extends StatefulWidget {
   /// 小数点长度[keyboardType]类型为[KeyboardType.decimal]或[KeyboardType.decimalNegative]生效
   final int decimalLength;
 
-  /// 最大输入值
-  final double? maxDecimal;
+  /// 整数位长度
+  final int? integerLength;
 
   /// 装饰是否与输入字段大小相同、该属性用于修改高度
   final bool isCollapsed;
@@ -174,7 +174,7 @@ class SuperTextFiled extends StatefulWidget {
     this.errorColor = const Color(0xFFFF0000),
     this.borderWidth = 1.0,
     this.decimalLength = 2,
-    this.maxDecimal,
+    this.integerLength,
     this.textInputAction,
     this.inputFormatters,
     this.maxLength,
@@ -258,11 +258,11 @@ class _SuperTextFiledState extends State<SuperTextFiled> {
         break;
       case KeyboardType.decimal:
         textInputType = const TextInputType.numberWithOptions(decimal: true);
-        formatter.add(NumberInputFormatter(decimal: widget.decimalLength, max: widget.maxDecimal));
+        formatter.add(XNumberTextInputFormatter(maxDecimalLength: widget.decimalLength, maxIntegerLength: widget.integerLength, isAllowNegative: false));
         break;
       case KeyboardType.decimalNegative:
         textInputType = const TextInputType.numberWithOptions(decimal: true, signed: true);
-        formatter.add(NumberInputFormatter(decimal: widget.decimalLength, max: widget.maxDecimal, decimalNegative: true));
+        formatter.add(XNumberTextInputFormatter(maxDecimalLength: widget.decimalLength, maxIntegerLength: widget.integerLength, isAllowNegative: true));
         break;
       default:
         textInputType = TextInputType.text;
@@ -412,51 +412,102 @@ class _SuperTextFiledState extends State<SuperTextFiled> {
   }
 }
 
-class NumberInputFormatter extends TextInputFormatter {
-  final double? max;
-  final int decimal;
-  final bool decimalNegative;
-  late final RegExp _exp;
+class XNumberTextInputFormatter extends TextInputFormatter {
+  final int? _maxIntegerLength;
+  final int? _maxDecimalLength;
+  final bool _isAllowDecimal;
+  final bool _isAllowNegative;
 
-  NumberInputFormatter({
-    this.max,
-    this.decimal = 2,
-    this.decimalNegative = false,
-  }) {
-    if (decimal == 0) {
-      _exp = decimalNegative ? RegExp(r'(^[-1-9](\d+)?$)|(^(0)$)') : RegExp(r'(^[1-9](\d+)?$)|(^(0)$)');
-    } else {
-      _exp = RegExp(
-        decimalNegative
-            ? r'(^[-1-9](\d+)?$)|(^(0)$)|(^(0\.)(\d+)?(\d+)?$)|(^[-1-9](\d+)?(\.)?(\d+)?$)'
-            : r'(^[1-9](\d+)?$)|(^(0)$)|(^(0\.)(\d+)?(\d+)?$)|(^[1-9](\d+)?(\.)?(\d+)?$)',
-      );
-    }
-  }
+  /// [maxIntegerLength]限定整数的最大位数，为null时不限
+  /// [maxDecimalLength]限定小数点的最大位数，为null时不限
+  /// [isAllowDecimal]是否可以为小数，默认是可以为小数，也就是可以输入小数点
+  /// [isAllowNegative]是否可以为负数
+  XNumberTextInputFormatter({
+    int? maxIntegerLength,
+    int? maxDecimalLength,
+    bool isAllowDecimal = true,
+    bool isAllowNegative = true,
+  })  : _maxIntegerLength = maxIntegerLength,
+        _maxDecimalLength = maxDecimalLength,
+        _isAllowDecimal = isAllowDecimal,
+        _isAllowNegative = isAllowNegative;
 
   @override
-  TextEditingValue formatEditUpdate(
-    TextEditingValue oldValue,
-    TextEditingValue newValue,
-  ) {
-    final input = newValue.text;
-    if (input.isEmpty) return newValue;
-    if (!_exp.hasMatch(input)) return oldValue;
-    if (max != null) {
-      final num = double.tryParse(input) ?? 0;
-      if (num > max!) {
-        return TextEditingValue(
-          text: max.toString(),
-          selection: TextSelection.collapsed(offset: max.toString().length),
-        );
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    String value = newValue.text.trim(); //去掉前后空格
+    int selectionIndex = newValue.selection.end;
+    if (_isAllowDecimal) {
+      if (value == '.') {
+        value = '0.';
+        selectionIndex++;
+      } else if (value != '' && _isToDoubleError(value)) {
+        //不是double输入数据
+        return _oldTextEditingValue(oldValue);
+      }
+      //包含小数点
+      if (value.contains('.')) {
+        int pointIndex = value.indexOf('.');
+        String beforePoint = value.substring(0, pointIndex);
+
+        String afterPoint = value.substring(pointIndex + 1, value.length);
+        //小数点前面没内容补0
+        if (beforePoint.isEmpty) {
+          value = '0.$afterPoint';
+          selectionIndex++;
+        } else {
+          //限定整数位数
+          if (_maxIntegerLength != null) {
+            if (beforePoint.length > _maxIntegerLength!) {
+              return _oldTextEditingValue(oldValue);
+            }
+          }
+        }
+        //限定小数点位数
+        if (_maxDecimalLength != null) {
+          if (afterPoint.length > _maxDecimalLength!) {
+            return _oldTextEditingValue(oldValue);
+          }
+        }
+      } else {
+        //限定整数位数
+        if (_maxIntegerLength != null) {
+          if (value.length > _maxIntegerLength!) {
+            return _oldTextEditingValue(oldValue);
+          }
+        }
+      }
+    } else {
+      if (value.contains('.') || (value != '' && _isToDoubleError(value)) || (_maxIntegerLength != null && value.length > _maxIntegerLength!)) {
+        return _oldTextEditingValue(oldValue);
       }
     }
-    if (decimal > 0) {
-      final index = input.indexOf('.');
-      if (index < 0) return newValue;
-      final afterDot = input.substring(index, input.length).length - 1;
-      if (afterDot > decimal) return oldValue;
+
+    return TextEditingValue(
+      text: value,
+      selection: TextSelection.collapsed(offset: selectionIndex),
+    );
+  }
+
+  ///返回旧的输入内容
+  TextEditingValue _oldTextEditingValue(TextEditingValue oldValue) {
+    return TextEditingValue(
+      text: oldValue.text,
+      selection: TextSelection.collapsed(offset: oldValue.selection.end),
+    );
+  }
+
+  ///输入内容不能解析成double
+  bool _isToDoubleError(String value) {
+    if (!_isAllowNegative) {
+      if (value.contains('-')) {
+        return true;
+      }
     }
-    return newValue;
+    try {
+      double.parse(value);
+    } catch (e) {
+      return true;
+    }
+    return false;
   }
 }
